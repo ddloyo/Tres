1ec843cfa41c2db41a0856470f5d6e814bfc0b55
Diego Diaz <49206961+ddloyo@users.noreply.github.com>
2025-06-03 14:18:07 +0000

    set of basics

--- lib/logic/deck_logic.dart
+++ lib/logic/deck_logic.dart
@@ -1,1 +1,17 @@
-// deck_logic
+import 'dart:math';
+import '../models/card_model.dart';
+
+List<CardModel> generateDeck() {
+  List<CardModel> deck = [];
+  int idCounter = 0;
+
+  for (int value = 1; value <= 12; value++) {
+    for (int copy = 0; copy < 3; copy++) {
+      deck.add(CardModel(value: value, id: 'c$idCounter'));
+      idCounter++;
+    }
+  }
+
+  deck.shuffle(Random());
+  return deck;
+}
--- lib/logic/player_logic.dart
+++ lib/logic/player_logic.dart
@@ -1,1 +1,21 @@
-// deck_logic
+
+import '../models/player_model.dart';
+
+List<PlayerModel> createPlayers(int count) {
+  return List.generate(count, (index) {
+    return PlayerModel(
+      id: 'player_$index',
+      name: 'Jugador ${index + 1}',
+      hand: [],
+    );
+  });
+}
+
+void dealCards(List<CardModel> deck, List<PlayerModel> players) {
+  for (int i = 0; i < 5; i++) {
+    for (var player in players) {
+      player.hand.add(deck.removeLast());
+    }
+  }
+}
+
